dacea4181f302a78846da4a94b9a3e9f5fb9a994
Jon Roig <jon@jonroig.com>
2015-07-22 15:57:00 -0700

    blah

--- Completed/server.js
+++ Completed/server.js
@@ -3,15 +3,15 @@
 */
 
 var express   	= require('express');
-var app        	= express(); 
+var app        	= express();
 var morgan 			= require('morgan');
-var path 				= require('path'); 
+var path 				= require('path');
 
-// Initialize variables. 
-var port = process.env.PORT || 8080; 
+// Initialize variables.
+var port = process.env.PORT || 80;
 
 // Configure morgan module to log all requests.
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 
 // Set the front-end folder to serve public assets.
 app.use(express.static(__dirname + '/frontend'));
@@ -21,6 +21,6 @@ app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/frontend/views/index.html'));
 });
 
-// Start the server.  
+// Start the server.
 app.listen(port);
-console.log('Listening on port ' + port + '...'); 
+console.log('Listening on port ' + port + '...');
